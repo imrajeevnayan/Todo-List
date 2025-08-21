9f7fb320a8a818f600e5ae7ad92918934417228e
imrajeevnayan <immrrobot121@gmail.com>
2025-08-21 15:43:42 +0530

    Update Dockerfile & readme.md and Todoapplication.java

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -2,6 +2,9 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoApplication {
@@ -10,5 +13,16 @@ public class TodoApplication {
 		SpringApplication.run(TodoApplication.class, args);
         System.out.println("running Successfully");
 	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://todo-list-fo0g.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 
 }
